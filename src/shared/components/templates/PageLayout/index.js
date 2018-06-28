396cba714e2dad84b5ec16bf7891b286d23f5fb1
daint2git <daint2dev@gmail.com>
2018-06-29 00:50:44 +0700

    Update component

--- src/shared/components/templates/PageLayout/index.js
+++ src/shared/components/templates/PageLayout/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavigationBar from 'shared/components/molecules/NavigationBar'
 
+import loadStyles from 'shared/components/utils/loadStyles'
+import styles from './styles.scss'
+
+const loadClass = loadStyles(styles)
+
 export default function PageLayout(props) {
   const { children } = props
   return (
-    <div className="root">
+    <div className={loadClass(`root`)}>
       <div className="header"><h1>HEADER</h1></div>
       <div className="navigationBar"><NavigationBar /></div>
       <div className="content">{children}</div>
