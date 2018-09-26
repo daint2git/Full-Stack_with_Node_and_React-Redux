3196f13845d518d24fe48b2cef93ed695486421a
daint2git <peterpoy93@gmail.com>
2018-09-26 22:40:53 +0700

    [FE] add cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const cors = require('cors')
 
 const accountsRouter = require('./routes/accounts')
 const productsRouter = require('./routes/products')
@@ -17,11 +18,10 @@ app.use(express.urlencoded({ extended: true }))
 // for parsing multipart/form-data
 app.use(upload.array())
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:6969',
+}))
 
 app.use('/api/accounts', accountsRouter)
 app.use('/api/products', productsRouter)
