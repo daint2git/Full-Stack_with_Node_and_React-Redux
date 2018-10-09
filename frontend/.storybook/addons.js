27391ec1653bbf3fce3c5887bb43eba0ed33c4b6
daint2git <peterpoy93@gmail.com>
2018-10-09 23:53:27 +0700

    [FE]_add config storybook

--- frontend/.storybook/addons.js
+++ frontend/.storybook/addons.js
@@ -1,2 +1,3 @@
 import '@storybook/addon-storysource/register'
+import '@storybook/addon-backgrounds/register'
 import '@storybook/addon-options/register'
--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,8 +1,18 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
+import addonBackgrounds from '@storybook/addon-backgrounds'
 import { setOptions } from '@storybook/addon-options'
 
 import 'shared/components/utils/root.scss'
 import 'shared/components/utils/fortawesomeLoader.js'
+
+import './styles.scss'
+
+const backgrounds = addonBackgrounds([
+  { name: 'white', value: '#FFF', default: true },
+  { name: 'black', value: '#000' },
+])
+
+addDecorator(backgrounds)
 
 setOptions({
   hierarchyRootSeparator: /\|/,
